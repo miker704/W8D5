3ca59e56569f7ae5a4c09971095a38b28c1f06a3
miker704 <33719996+miker704@users.noreply.github.com>
2022-05-20 16:18:52 -0400

    Update protype_inherritance.js

--- src/prototypal_inheritance/protype_inherritance.js
+++ src/prototypal_inheritance/protype_inherritance.js
@@ -1,33 +1,57 @@
 
 
-Function.prototype.inherits = function(cxt) {
+Function.prototype.inherits = function (obj) {
+    // function Surrogate () { }
+    // Surrogate.prototype = obj.prototype;
+    // this.prototype = new Surrogate();
+    // this.prototype.constructor = this;
+    this.prototype = Object.create(obj.prototype);
 
+};
+
+
+function MovingObject (speed) {
+
+    this.speed = speed;
 
 }
 
+// MovingObject.prototype.getSpeed = function () {
+
+//     // console.log(this.speed);
+//     return this.speed;
+
+// }
 
 
-
-function MovingObject () {}
-
-function Ship () {}
+function Ship () { 
+    // MovingObject.prototype.getSpeed.call(this);
+}
 Ship.inherits(MovingObject);
+let ship = new Ship();
+// console.log(MovingObject.getSpeed.call(ship));
+// console.log(ship)
+// console.log(ship.getSpeed())
+ship.speed =20;
+console.log(ship.speed)
 
-function Asteroid () {}
+
+
+function Asteroid () { }
 Asteroid.inherits(MovingObject);
 
 
 
 // How would you test Function.prototype.inherits? A few specs to consider:
 
-// You should be able to define methods/attributes 
+// You should be able to define methods/attributes
 // on MovingObject that ship and asteroid instances can use.
-// Defining a method on Asteroid's 
+// Defining a method on Asteroid's
 // prototype should not mean that a ship can call it.
-// Adding to Ship or Asteroid's prototypes should 
+// Adding to Ship or Asteroid's prototypes should
 // not change MovingObject's prototype.
-// The Ship and Asteroid prototypes should not be instances 
+// The Ship and Asteroid prototypes should not be instances
 // of MovingObject.
-// After you have written Function.prototype.inherits 
+// After you have written Function.prototype.inherits
 // using the surrogate trick, refactor your solution using Object.create.
 //  Make sure to test that your updated solution works.
