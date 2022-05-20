1d21e8341417433c1366392fc845ab9bb1b26e1a
miker704 <miker704@gmail.com>
2022-05-20 10:33:02 -0400

    finished sum and binded args

--- src/arguments_exercises/bind_with_args.js
+++ src/arguments_exercises/bind_with_args.js
@@ -1,44 +1,91 @@
+Function.prototype.myBind = function (context) {
+    // console.log(context);
+    const that = this;
+    const bindArgs = Array.from(arguments).slice(1);
+    return function bounded_function () {
+        const callarg = Array.from(arguments);
+        that.apply(context, bindArgs.concat(callarg));
+    };
+};
+
+Function.prototype.myBind_1 = function (ctx, ...bindArgs) {
+
+    const that = this;
+    return (...prevArgs) => that.apply(ctx, bindArgs.concat(prevArgs));
+
+};
+
 class Cat {
-    constructor(name) {
-      this.name = name;
+    constructor (name) {
+        this.name = name;
     }
-  
-    says(sound, person) {
-      console.log(`${this.name} says ${sound} to ${person}!`);
-      return true;
+
+    says (sound, person) {
+        console.log(`${this.name} says ${sound} to ${person}!`);
+        return true;
     }
-  }
-  
-  class Dog {
-    constructor(name) {
-      this.name = name;
+}
+
+class Dog {
+    constructor (name) {
+        this.name = name;
     }
-  }
-  
-  const markov = new Cat("Markov");
-  const pavlov = new Dog("Pavlov");
-  
-  markov.says("meow", "Ned");
-  // Markov says meow to Ned!
-  // true
-  
-  // bind time args are "meow" and "Kush", no call time args
-  markov.says.myBind(pavlov, "meow", "Kush")();
-  // Pavlov says meow to Kush!
-  // true
-  
-  // no bind time args (other than context), call time args are "meow" and "a tree"
-  markov.says.myBind(pavlov)("meow", "a tree");
-  // Pavlov says meow to a tree!
-  // true
-  
-  // bind time arg is "meow", call time arg is "Markov"
-  markov.says.myBind(pavlov, "meow")("Markov");
-  // Pavlov says meow to Markov!
-  // true
-  
-  // no bind time args (other than context), call time args are "meow" and "me"
-  const notMarkovSays = markov.says.myBind(pavlov);
-  notMarkovSays("meow", "me");
-  // Pavlov says meow to me!
-  // true
+}
+
+
+console.log("using binded function without ... op " + '\n');
+
+
+
+const markov = new Cat("Markov");
+const pavlov = new Dog("Pavlov");
+
+markov.says("meow", "Ned");
+// Markov says meow to Ned!
+// true
+
+// bind time args are "meow" and "Kush", no call time args
+markov.says.myBind(pavlov, "meow", "Kush")();
+// Pavlov says meow to Kush!
+// true
+
+// no bind time args (other than context), call time args are "meow" and "a tree"
+markov.says.myBind(pavlov)("meow", "a tree");
+// Pavlov says meow to a tree!
+// true
+
+// bind time arg is "meow", call time arg is "Markov"
+markov.says.myBind(pavlov, "meow")("Markov");
+// Pavlov says meow to Markov!
+// true
+
+// no bind time args (other than context), call time args are "meow" and "me"
+const notMarkovSays = markov.says.myBind(pavlov);
+notMarkovSays("meow", "me");
+// Pavlov says meow to me!
+// true
+console.log('\n');
+console.log("using binded function with ... op " + '\n');
+
+
+markov.says("meow", "Ned");
+// Markov says meow to Ned!
+// true  
+// bind time args are "meow" and "Kush", no call time args
+markov.says.myBind_1(pavlov, "meow", "Kush")();
+// Pavlov says meow to Kush!
+// true
+
+// no bind time args (other than context), call time args are "meow" and "a tree"
+markov.says.myBind_1(pavlov)("meow", "a tree");
+// Pavlov says meow to a tree!
+// true
+
+// bind time arg is "meow", call time arg is "Markov"
+markov.says.myBind_1(pavlov, "meow")("Markov");
+// Pavlov says meow to Markov!
+// true
+
+// no bind time args (other than context), call time args are "meow" and "me"
+const notMarkovSays1 = markov.says.myBind_1(pavlov);
+notMarkovSays("meow", "me");
--- src/arguments_exercises/sum.js
+++ src/arguments_exercises/sum.js
@@ -1,6 +1,25 @@
+console.log("sum function with out ... operator");
+
+function sum () {
+    let res = 0;
+     let args = Array.from(arguments);
+     for(let num of args){
+         res+=num;
+     }
+    return res;
+}
 
 
-function sum (...arguments) {
+
+
+console.log(sum(1, 2, 3, 4));    //=== 10)
+console.log(sum(1, 2, 3, 4, 5));   //=== 15)
+
+
+console.log("sum function with ... operator");
+
+
+function sum_s (...arguments) {
     let res = 0;
     for (let arg of arguments) {
         res += arg;
@@ -10,5 +29,5 @@ function sum (...arguments) {
 }
 
 
-console.log(sum(1, 2, 3, 4));    //=== 10)
-console.log(sum(1, 2, 3, 4, 5));   //=== 15)
+console.log(sum_s(1, 2, 3, 4));    //=== 10)
+console.log(sum_s(1, 2, 3, 4, 5));   //=== 15)
